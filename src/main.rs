95ed69a43b12c59f7b9b91bb5c2528626715eba5
SumOfAllN00bs <discordshards@gmail.com>
2020-06-29 05:32:36 +1200

    Caesar seems to work

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 fn main() {
-    //caesar("test phrase", 10);
-    caesar("TEST PHRASE", 10);
-    //println!("{}", ((-100 % 26) + 26) % 26);
+    // println!("{}", caesar("test phrase", 10));
+    // println!("{}", caesar("TEST PHRASE", 10));
+    // println!("{}", ((-100 % 26) + 26) % 26);
 }
 
 fn caesar(_input: &str, _shift: i32) -> String{
-    let _upper: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let _lower: &str = "abcdefghijklmnopqrstuvwxyz";
     let mut _result: String = String::new();
-    let trueshift = ((_shift % 26) + 26) % 26;
+    let mut _trueshift: u32 = (((_shift % 26) + 26) % 26) as u32;
 
     for c in _input.chars() {
         let converted = c as u32;
-        if converted <= 64 || converted >= 123 {
+        if !(converted >= 65 && converted <= 90) && !(converted >= 97 && converted <= 122) {
             _result.push(std::char::from_u32(converted).unwrap());
             continue;
         }
         if converted >= 65 && converted <= 90 {
-
+            _result.push(std::char::from_u32((((converted - 65) + _trueshift) % 26) + 65).unwrap());
+        }
+        if converted >= 97 && converted <= 122 {
+            _result.push(std::char::from_u32((((converted - 97) + _trueshift) % 26) + 97).unwrap());
         }
     }
 
